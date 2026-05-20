61ce8e3b1f98dc62aa308aeef63ba46f5d26f9cc
AyTG67 <elitetg67@gmail.com>
2026-05-20 21:44:48 +0500

    chore: downgrade Kotlin plugin and stdlib to cached 2.0.21 to prevent network hangs

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -51,10 +51,10 @@ configurations.all {
         force("androidx.browser:browser:1.8.0")
         force("androidx.core:core:1.13.1")
         force("androidx.core:core-ktx:1.13.1")
-        force("org.jetbrains.kotlin:kotlin-stdlib:2.1.0")
-        force("org.jetbrains.kotlin:kotlin-stdlib-jdk7:2.1.0")
-        force("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.1.0")
-        force("org.jetbrains.kotlin:kotlin-stdlib-common:2.1.0")
+        force("org.jetbrains.kotlin:kotlin-stdlib:2.0.21")
+        force("org.jetbrains.kotlin:kotlin-stdlib-jdk7:2.0.21")
+        force("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.0.21")
+        force("org.jetbrains.kotlin:kotlin-stdlib-common:2.0.21")
 
     }
 }
